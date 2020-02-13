b7998cbdc0de7b8c4c870e4f25022b44999c0459
build-failure <michael@lewkowski.de>
2020-02-13 07:33:42 +0100

    Add module init test; state: absent test

--- filter_plugins/tests/test_network_encryption.py
+++ filter_plugins/tests/test_network_encryption.py
@@ -5,6 +5,13 @@ from filter_plugins.network_encryption import *
 
 class NetworkEncryptionTest(unittest.TestCase):
 
+    def test_init(self):
+        module = FilterModule()
+        filters = module.filters()
+
+        self.assertEqual(filters.get('network_encryption_point_to_point_connections'), network.get_point_to_point_connections)
+        self.assertEqual(filters.get('network_encryption_configs'), get_network_encryption_configs)
+
     def test_get_network_encryption_configs(self):
         configs = [{
             'name': 'default',
@@ -26,4 +33,28 @@ class NetworkEncryptionTest(unittest.TestCase):
             'psk': 'secret',
             'hosts': ['one'],
             'absent_hosts': []
+        }])
+
+    def test_get_network_encryption_configs_absent(self):
+        configs = [{
+            'name': 'default',
+            'psk': 'secret'
+        }]
+        play_hosts = ['one']
+        hostvars = {
+            'one' : {
+                'network_encryption_host_configs': [{
+                    'name': 'default',
+                    'state': 'absent'
+                }]
+            }
+        }
+
+        result = get_network_encryption_configs(configs, play_hosts, hostvars)
+
+        self.assertEqual(result, [{
+            'name': 'default',
+            'psk': 'secret',
+            'hosts': [],
+            'absent_hosts': ['one']
         }])
